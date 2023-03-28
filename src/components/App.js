fef13b36b3727ff727bed315bdb2774ddc329891
SUMAN GHOSH <90588892+sumansingle@users.noreply.github.com>
2023-03-28 10:48:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,48 @@
 
 import '../styles/App.css';
-import React, { useState, useEffect } from 'react';
+import React, { Component, useState, useEffect } from "react";
 
-function App() {
-  const [countdown, setCountdown] = useState(0);
-  const [timer, setTimer] = useState(null);
-  const [inputValue, setInputValue] = useState('');
+const App = () => {
+  // write your code here 
+  const [counter, setCounter] = useState(0)
+
+  var changeCounter = (e) => {
+    if (e.key === "Enter") {
+      if (isNaN(e.target.value) || e.target.value === '') {
+        setCounter(0);
+      } else {
+        setCounter(parseInt(e.target.value));
+      }
+    }
+  };
 
   useEffect(() => {
-    if (countdown === 0 && timer) {
-      clearInterval(timer);
-      setTimer(null);
-    }
-  }, [countdown, timer]);
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      const value = Math.floor(parseFloat(inputValue));
-      if (isNaN(value) || value < 0) {
-        setCountdown(0);
-        if (timer) {
-          clearInterval(timer);
-          setTimer(null);
-        }
-      } else {
-        setCountdown(value);
-        if (timer) {
-          clearInterval(timer);
-        }
-        setTimer(setInterval(() => setCountdown((c) => c - 1), 1000));
+    const i = setInterval(() => {
+      if (counter === 0) {
+        setCounter(0);
       }
-      setInputValue('');
-    }
-  };
+      if (counter !== 0) {
+        setCounter(counter - 1);
+      }
+    }, 1000);
 
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
+    return () => {
+      clearInterval(i);
+    };
+  }, [counter]);
+
 
   return (
-    <div>
-      <input type="text" id="timeCount" value={inputValue} onChange={handleChange} onKeyDown={handleKeyDown} />
-      <div id="current-time">{countdown}</div>
+    <div className="wrapper">
+      <div id="whole-center">
+        <h1>
+          Reverse countdown for<input id="timeCount" onKeyDown={changeCounter} /> sec.
+        </h1>
+      </div>
+      <div id="current-time">{counter}</div>
     </div>
-  );
+  )
 }
+
 
 export default App;
